feb18310f97baa59411f02ffabd3cad75823d697
Gabriel Naiman <gabynaiman@gmail.com>
2014-02-02 15:57:12 -0300

    Refactor repository namespace and files

--- lib/veins/repository/queriable.rb
+++ lib/veins/repository/queriable.rb
@@ -1,24 +1,26 @@
 module Veins
-  class Queriable
+  class Repository
+    class Queriable
 
-    include Enumerable
+      include Enumerable
 
-    def initialize(queriable, query=nil, &block)
-      @queriable = queriable
-      @query = query || block
+      def initialize(queriable, query=nil, &block)
+        @queriable = queriable
+        @query = query || block
+      end
+
+      def query(query=nil, &block)
+        Queriable.new self, query || block
+      end
+
+      def to_a
+        @query.call(@queriable).to_a
+      end
+
+      def each(&block)
+        to_a.each &block
+      end
+
     end
-
-    def query(query=nil, &block)
-      Queriable.new self, query || block
-    end
-
-    def to_a
-      @query.call(@queriable).to_a
-    end
-
-    def each(&block)
-      to_a.each &block
-    end
-
   end
 end
